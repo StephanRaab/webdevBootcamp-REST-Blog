8556a2d517019cfe6e830431cc078f41f7a3f35b
StephanRaab <sarcartist@gmail.com>
2016-12-03 17:24:44 -0500

    show route

--- app.js
+++ app.js
@@ -53,6 +53,17 @@ app.post("/blogs", function(req, res){
   });
 });
 
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show.ejs", {blog: foundBlog});
+    }
+  })
+})
+
 app.listen(3000, function(){
   console.log("RESTful Blog App running on localhost:3000");
 })
